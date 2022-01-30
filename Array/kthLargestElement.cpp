c143af01f1ab9d198e33562089f1e1d07080f8df
Kunal Kashyap <34695022+kunalkashyap855@users.noreply.github.com>
2022-01-30 01:51:38 -0500

    Create kthLargestElement.cpp

---
+++ Array/kthLargestElement.cpp
@@ -0,0 +1,27 @@
+/*
+    Given an integer array nums and an integer k, return the kth largest element in the array.
+
+    Note that it is the kth largest element in the sorted order, not the kth distinct element.
+*/
+
+int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int> pq;
+
+    for(int i = 0; i < nums.size(); i++) {
+        pq.push(nums[i]);
+    }
+
+
+    for(int i = 0; i < k-1; i++) {
+        pq.pop();
+    }
+
+    return pq.top();
+}
+
+/*
+    We can simply sort the array and return the (n-k)th element. Time complexity for this approach will be O(nlogn).
+    
+    We can improve this time complexity by using Priority Queue as done here.
+    Time Complexity = O(n + klogn)
+*/
