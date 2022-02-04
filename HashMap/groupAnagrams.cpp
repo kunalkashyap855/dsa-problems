fa688855d26d84be4dd3b07c2f02e1baf7df8cb5
Kunal Kashyap <34695022+kunalkashyap855@users.noreply.github.com>
2022-02-04 16:10:28 -0500

    Create groupAnagrams.cpp

---
+++ HashMap/groupAnagrams.cpp
@@ -0,0 +1,27 @@
+/*
+    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+    Example 1:
+      Input: strs = ["eat","tea","tan","ate","nat","bat"]
+      Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+*/
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> ans;
+    unordered_map<string, vector<string>> umap;
+
+    for(int i = 0; i < strs.size(); i++) {
+        string temp = strs[i];
+        sort(strs[i].begin(), strs[i].end());
+        umap[strs[i]].push_back(temp);
+    }
+
+    for(auto itr=umap.begin(); itr != umap.end(); ++itr) {
+        ans.push_back(itr->second);
+    }
+
+    return ans;
+}
+
+// Time Complexity = O(nklogk) <= klogk is because of sorting the longest string of length k and n is because of for loop.
