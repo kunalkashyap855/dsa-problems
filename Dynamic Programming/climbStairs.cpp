b200902b8d085881a70bc7a43342054a7d09831b
Kunal Kashyap <34695022+kunalkashyap855@users.noreply.github.com>
2022-02-10 11:01:31 -0500

    Create climbStairs.cpp

---
+++ Dynamic Programming/climbStairs.cpp
@@ -0,0 +1,39 @@
+/*
+    You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+    Example 1:
+      Input: n = 2
+      Output: 2
+      Explanation: There are two ways to climb to the top.
+      1. 1 step + 1 step
+      2. 2 steps
+*/
+
+// Recursive Solution => O(n^2)
+int climbStairs(int n) {
+  if(n <= 2)
+    return n;
+  
+  return climbStairs(n-1) + climbStairs(n-2);
+}
+
+
+// DP Solution => O(n)
+int climbStairs(int n) {
+    if(n <= 2)
+        return n;
+
+    int dp[n+1];
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+}
+
+// Time Complexity = O(n)
